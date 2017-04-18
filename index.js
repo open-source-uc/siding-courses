b439997b3d71a9c667caeb4ec10a2c2f533b317d
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-04-18 13:46:10 -0300

    export as function

--- index.js
+++ index.js
@@ -4,25 +4,32 @@ const utils = require('./libs/utils');
 const YearSelect = require('./libs/YearSelect');
 const YearCatalogue = require('./libs/YearCatalogue');
 
-const delay = process.env.DELAY_MS || 3000;
-const username = process.env.SIDING_USERNAME;
-const password = process.env.SIDING_PASSWORD;
+function scrap(username, password, delay, dir) {
+  delay = delay || 3000;
+  dir = dir || './data.json';
 
-logIn(username, password).then(navigator => {
-  navigator.visit(utils.catalogueIndexUrl).then(res => {
-    const catalogues = [];
-    const yearSelect = new YearSelect(res.body);
-    yearSelect.urls.forEach((url, index) => {
-      setTimeout(() => catalogues.push(new YearCatalogue(navigator, url)), delay * index);
-    });
+  return new Promise((resolve, reject) => {
+    logIn(username, password).then(navigator => {
+      navigator.visit(utils.catalogueIndexUrl).then(res => {
+        const catalogues = [];
+        const yearSelect = new YearSelect(res.body);
+        yearSelect.urls.forEach((url, index) => {
+          setTimeout(() => catalogues.push(new YearCatalogue(navigator, url)), delay * index);
+        });
 
-    setTimeout(() => {
-      Promise.all(catalogues).then(courses => {
-        const flatCourses = [].concat(...courses);
-        const dir = './data.json';
-        jsonfile.writeFileSync(dir, flatCourses);
-        console.log('Results stored in data.json');
+        setTimeout(() => {
+          Promise.all(catalogues).then(courses => {
+            const flatCourses = [].concat(...courses);
+            jsonfile.writeFileSync(dir, flatCourses);
+            console.log('Results stored in data.json');
+            resolve();
+          });
+        }, delay * (yearSelect.urls.length + 1));
       });
-    }, delay * (yearSelect.urls.length + 1))
+    }).catch(err => {
+      reject(err);
+    });
   });
-});
+}
+
+module.exports = scrap;
